f7ade9748c4300be9085cb9cf8c97d9510f717d1
Member Four <member4@elton.com>
2025-09-17 19:40:20 +0800

    feat(leader): add lib/main.dart to run all member demos

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+﻿ // lib/main.dart
+ import 'demo_member2.dart' as m2;
+ import 'demo_member3.dart' as m3;
+ import 'demo_member4.dart' as m4;
+
+ void main() {
+   print('=== All Demos ===');
+   m2.runDemo();
+   m3.runDemo();
+   m4.runDemo();
+ }
