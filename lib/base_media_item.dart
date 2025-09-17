bc54e82bc37d63e631c04865fd78993351643352
ayet <member3@ayet.com>
2025-09-17 18:58:48 +0800

    WIP: local changes before rebase

---
+++ lib/base_media_item.dart
@@ -0,0 +1,31 @@
+class MediaItem {
+  int id;
+  String title;
+  double _rating;
+
+  MediaItem({
+    required this.id,
+    required this.title,
+    double rating = 0.0,
+  }) : _rating = rating;
+
+  double get rating => _rating;
+
+  set rating(double value) {
+    if (value < 0 || value > 10) {
+      throw ArgumentError('Rating must be between 0 and 10.');
+    }
+    _rating = value;
+  }
+
+  void updateRating(double newRating) {
+    rating = newRating;
+    print('Updated rating to $_rating');
+  }
+
+  void playPreview() {
+    print('Playing generic media preview...');
+  }
+
+  String info() => 'MediaItem(id:$id, title:$title, rating:$_rating)';
+}
